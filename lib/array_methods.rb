6caf3097e98189105504661c704924914349cb80
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-21 00:02:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.index (value_to_find)
+  array.index(value_to_find)
 end
 
-def find_max_value(array){
-  array.length
-  puts array
-}
+def find_max_value(array)
 end
 
 def find_min_value(array)
