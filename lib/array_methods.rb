068aa8898c96e7a998797bda6c44dc12ba810ec3
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-21 01:33:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,9 +4,12 @@ end
 
 def find_max_value(array)
 counter = 0
-length = array.length
 
-l
+while counter < array length do
+  puts array[counter]
+  counter += 1
+end
+
 end
 
 def find_min_value(array)
